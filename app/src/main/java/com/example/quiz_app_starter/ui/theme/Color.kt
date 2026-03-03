9bc6af2f3900f7a8befcc108730799b36271e0fd
CoOkieDragOnJuliaS <julia.sass@stud.hcw.ac.at>
2026-03-03 19:44:10 +0100

    [Session Codelab] feature: Changing color coding for better visualizing

--- app/src/main/java/com/example/quiz_app_starter/ui/theme/Color.kt
+++ app/src/main/java/com/example/quiz_app_starter/ui/theme/Color.kt
@@ -5,11 +5,28 @@ import androidx.compose.ui.graphics.Color.Companion.Cyan
 import androidx.compose.ui.graphics.Color.Companion.Green
 
 //Hier ändern und dann laden
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light theme
+val LightPrimary = Color(0xFF6200EE)
+val LightPrimaryVariant = Color(0xFF3700B3)
+val LightSecondary = Color(0xFF03DAC5)
+val LightBackground = Color(0xFFFFFFFF)
+val LightSurface = Color(0xFFFFFFFF)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightOnSecondary = Color(0xFF000000)
+val LightOnBackground = Color(0xFF000000)
+val LightOnSurface = Color(0xFF000000)
 
+// Dark theme
+val DarkPrimary = Color(0xFFBB86FC)
+val DarkPrimaryVariant = Color(0xFF3700B3)
+val DarkSecondary = Color(0xFF03DAC5)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF121212)
+val DarkOnPrimary = Color(0xFF000000)
+val DarkOnSecondary = Color(0xFF000000)
+val DarkOnBackground = Color(0xFFFFFFFF)
+val DarkOnSurface = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Teal accents
+val TealAccentLight = Color(0xFF4DB6AC)
+val TealAccentDark = Color(0xFF009688)
